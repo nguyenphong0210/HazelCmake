c8ea93e09ff8e0fe51e64eb2d8803ad620ae0668
Nguyen Phong <nguyenphong0210pt@gmail.com>
2025-09-18 00:00:36 +0700

    Added layers and layers Stack

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,11 +1,30 @@
 #include <Hazel.h>
 
+class ExampleLayer : public Hazel::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+		HZ_INFO("ExampleLayer::OnUpdate");
+	}
+
+	void OnEvent(Hazel::Event& e) override
+	{
+		HZ_TRACE("{0}", e);
+	}
+};
+
 class Sandbox : public Hazel::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
