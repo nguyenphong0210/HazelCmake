1ae08e709e938d2eed58f3bc3db0317f32273e42
Nguyen Phong <nguyenphong0210pt@gmail.com>
2025-10-19 14:48:58 +0700

    Refactor LayerStack to simplify layer management and ensure proper attachment/detachment handling

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -29,13 +29,11 @@ namespace Hazel {
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
-		layer->OnAttach();
 	}
 
 	void Application::PushOverlay(Layer* overlay)
 	{
 		m_LayerStack.PushOverlay(overlay);
-		overlay->OnAttach();
 	}
 
 	void Application::OnEvent(Event& e)
