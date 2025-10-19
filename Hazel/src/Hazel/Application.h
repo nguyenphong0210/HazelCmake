907b69153668e1432d5daf463229fab8bf9a7b8f
Nguyen Phong <nguyenphong0210pt@gmail.com>
2025-10-19 17:32:28 +0700

    Switched imgui to docking branch using their GLFW

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -7,6 +7,8 @@
 #include "Hazel/Events/Event.h"
 #include "Hazel/Events/ApplicationEvent.h"
 
+#include "Hazel/ImGui/ImGuiLayer.h"
+
 
 namespace Hazel {
 
@@ -28,7 +30,9 @@ namespace Hazel {
 		inline static Application& Get() { return *s_Instance; }
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
+		
 		std::unique_ptr<Window> m_Window;
+		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 		static Application* s_Instance;
