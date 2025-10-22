7a73fe1a37695140d1659e31f280395a76dba83f
Nguyen Phong <nguyenphong0210pt@gmail.com>
2025-10-22 23:31:58 +0700

    Implement OpenGL context management with GraphicsContext interface

---
+++ Hazel/src/Platform/OpenGL/OpenGLContext.h
@@ -0,0 +1,23 @@
+#ifndef OPENGLCONTEXT_H
+#define OPENGLCONTEXT_H
+
+#include "Hazel/Renderer/GraphicsContext.h"
+
+struct GLFWwindow;
+
+namespace Hazel {
+
+	class OpenGLContext : public GraphicsContext
+	{
+	public:
+		OpenGLContext(GLFWwindow* windowHandle);
+
+		virtual void Init() override;
+		virtual void SwapBuffers() override;
+	private:
+		GLFWwindow* m_WindowHandle;
+	};
+
+}
+
+#endif // OPENGLCONTEXT_H
